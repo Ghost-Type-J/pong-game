307ec768585b732aecc2a143967d0cc72ed2871b
ghost-type-j <107253394+ghost-type-j@users.noreply.github.com>
2023-09-15 11:24:06 +0100

    Updated main to include references to paddle class.
    Now we have user-input functioning paddles

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.setup(width=800, height=600)
+screen.tracer(0)
 
+paddle_r = Paddle((350, 0))
+paddle_l = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(paddle_r.move_up, "Up")
+screen.onkey(paddle_r.move_down, "Down")
+screen.onkey(paddle_l.move_up, "w")
+screen.onkey(paddle_l.move_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
